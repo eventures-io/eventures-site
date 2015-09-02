0df10b10f08f4aa217463e7fef07ca865acb0dbb
Peter Symoens <peter.symoens@eventures.io>
2015-09-02 17:03:30 +0100

    add dribble project

--- site/client/app/work/project-constants.js
+++ site/client/app/work/project-constants.js
@@ -6,7 +6,8 @@ angular.module('evtrs-site')
         polaroiz: {bgColor: '#F9F9F9', image: {src: 'assets/images/polaroiz.png', paddingTop: '50px'} },
         plantzr: {bgColor: '#606060', image: {src: 'assets/images/plantzr.jpg', paddingTop: '0px' }},
         plantzrMob: {bgColor: '#7d7d7d', image: {src: 'assets/images/plantzr-mobile.png', paddingTop: '20px' }},
-        oceanfeedr: {bgColor: '#c8c8c8', image: {src: 'assets/images/ocean-feedr.png', paddingTop: '20px'}}
+        oceanfeedr: {bgColor: '#c8c8c8', image: {src: 'assets/images/ocean-feedr.png', paddingTop: '20px'}},
+        dribble: {bgColor: '#a8a8a8', image: {src: 'assets/images/test4.gif', paddingTop: '180px'}}
 
 
     });
