0431119d834581361da4b822ef9450b06c0acd3b
Peter Symoens <peter.symoens@eventures.io>
2015-09-02 17:26:29 +0100

    add button overlay

--- site/client/app/work/project-constants.js
+++ site/client/app/work/project-constants.js
@@ -3,11 +3,9 @@
 angular.module('evtrs-site')
 
     .constant('PROJECT_CONSTANTS', {
-        polaroiz: {bgColor: '#F9F9F9', image: {src: 'assets/images/polaroiz.png', paddingTop: '50px'} },
-        plantzr: {bgColor: '#606060', image: {src: 'assets/images/plantzr.jpg', paddingTop: '0px' }},
-        plantzrMob: {bgColor: '#7d7d7d', image: {src: 'assets/images/plantzr-mobile.png', paddingTop: '20px' }},
         oceanfeedr: {bgColor: '#c8c8c8', image: {src: 'assets/images/ocean-feedr.png', paddingTop: '20px'}},
+        polaroiz: {bgColor: '#b0b0b0', image: {src: 'assets/images/polaroiz.png', paddingTop: '50px'} },
+        plantzrMob: {bgColor: '#7d7d7d', image: {src: 'assets/images/plantzr-mobile.png', paddingTop: '20px' }},
+        plantzr: {bgColor: '#606060', image: {src: 'assets/images/plantzr.jpg', paddingTop: '0px' }},
         dribble: {bgColor: '#a8a8a8', image: {src: 'assets/images/test4.gif', paddingTop: '180px'}}
-
-
     });
