b60a1a47970e8608791b618a748b62b3454a7bba
Peter Symoens <peter.symoens@eventures.io>
2015-09-03 17:07:48 +0100

    change color scheme

--- site/client/app/work/project-constants.js
+++ site/client/app/work/project-constants.js
@@ -3,9 +3,9 @@
 angular.module('evtrs-site')
 
     .constant('PROJECT_CONSTANTS', {
-        oceanfeedr: {bgColor: '#c8c8c8', image: {src: 'assets/images/ocean-feedr.png', paddingTop: '20px'}},
-        polaroiz: {bgColor: '#b0b0b0', image: {src: 'assets/images/polaroiz.png', paddingTop: '50px'} },
-        plantzrMob: {bgColor: '#7d7d7d', image: {src: 'assets/images/plantzr-mobile.png', paddingTop: '20px' }},
-        plantzr: {bgColor: '#606060', image: {src: 'assets/images/plantzr.jpg', paddingTop: '0px' }},
-        dribble: {bgColor: '#a8a8a8', image: {src: 'assets/images/test4.gif', paddingTop: '180px'}}
+        oceanfeedr: {bgColor: '#efefef', image: {src: 'assets/images/ocean-feedr.png', paddingTop: '20px'}},
+        polaroiz: {bgColor: '#efefef', image: {src: 'assets/images/polaroiz.png', paddingTop: '50px'} },
+        plantzrMob: {bgColor: '#efefef', image: {src: 'assets/images/plantzr-mobile.png', paddingTop: '20px' }},
+        plantzr: {bgColor: '#efefef', image: {src: 'assets/images/plantzr-menu.png', paddingTop: '40px' }},
+        dribble: {bgColor: '#efefef', image: {src: 'assets/images/logo1.png' , paddingTop: '100px'}}
     });
