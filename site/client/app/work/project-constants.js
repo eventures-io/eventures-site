66916b4f19ffab97a59727e9de4ae9145cf362a7
Peter Symoens <peter.symoens@eventures.io>
2015-11-06 21:32:22 +0000

    styling

--- site/client/app/work/project-constants.js
+++ site/client/app/work/project-constants.js
@@ -8,5 +8,5 @@ angular.module('evtrs-site')
         'plantzr-mobile': {bgColor: '#efefef', image: {src: 'assets/images/plantzr-mobile.png'}, outerHeight: '73vh', githubUrl: 'plantzr-mobile'},
         plantzr: {bgColor: '#efefef', image: {src: 'assets/images/plantzr.png'}, outerHeight: '80vh', siteUrl: 'http://plantzr.herokuapp.com', githubUrl: 'plantzr'},
         'angular-loader': {bgColor: '#efefef', image: {src: 'assets/images/angular-loader.png'}, outerHeight: '74vh', siteUrl: 'http://eventures-io.github.io/angular-loader-directive/', githubUrl: 'angular-loader-directive'},
-        'plantzr-framer': {bgColor: '#efefef', image: {src: 'assets/images/plantzr-nexus.png'}, outerHeight: '70vh', githubUrl: 'plantzr-mobile'}
+        'plantzr-framer': {bgColor: '#efefef', image: {src: 'assets/images/plantzr-nexus.png'}, outerHeight: '82vh', githubUrl: 'plantzr-mobile'}
     });
